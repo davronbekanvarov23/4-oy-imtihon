0359af4e61b7d005e3497c83cc8c28caaa8dfb67
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-03-09 20:46:33 +0500

    added

---
+++ app.js
@@ -0,0 +1,90 @@
+/*------------------1-masala----------------------*/
+// const text = "To be or not to be"
+// let pattern='not'
+// let newText=text.split(pattern)
+// console.log(newText)
+
+/*------------------3-masala----------------------*/
+// function numberfilter(num) {
+//   if (num % 2 == 0) {
+//     console.log('juft');
+//   } else {console.log('toq')}
+// }
+// numberfilter(15)
+
+/*------------------4-masala----------------------*/
+
+// const data = [
+//   { country: "Belarus", city: "Brest" },
+//   { country: "Russia", city: "Omsk" },
+//   { country: "Russia", city: "Samara" },
+//   { country: "Belarus", city: "Grodno" },
+//   { country: "Belarus", city: "Minsk" },
+//   { country: "Poland", city: "Lodz" },
+// ];
+
+// function createObjectByCountry(data) {
+//   const result = {};
+
+//   data.forEach((item) => {
+//     const country = item.country;
+//     const city = item.city;
+
+//     if (!result[country]) {
+//       result[country] = [];
+//     }
+
+//     result[country].push(city);
+//   });
+
+//   return result;
+// }
+
+// const result = createObjectByCountry(data);
+// console.log(result);
+
+/*------------------5-masala----------------------*/
+
+// function sumOfIntegersInRange(n1, n2) {
+//   let sum = 0;
+
+//     for (let i = n1; i <= n2; i++) {
+//       sum += i;
+//   }
+
+//   return sum;
+// }
+// const n1 = 3;
+// const n2 = 4;
+// const result = sumOfIntegersInRange(n1, n2);
+// console.log(result)
+
+/*------------------9-masala----------------------*/
+
+// const books = [
+//   {
+//     title: "Halqa",
+//     author: "Akrom Malik",
+//     alreadyRead: false,
+//   },
+//   {
+//     title: "Dunyo ishlari",
+//     author: "O'tkir Hoshimov",
+//     alreadyRead: true,
+//   },
+//   {
+//     title: "Vaqtning qadri",
+//     author: "Abdulfattoh Abu G'udda",
+//     alreadyRead: true,
+//   },
+// ];
+
+// books.forEach((book) => {
+//   if (book.alreadyRead == true) {
+//     console.log(`${book.author}ning ${book.title} kitobi oqilgan`);
+
+//   }
+//   else{console.log(`${book.author}ning ${book.title} kitobi oqilmagan`)}
+// });
+
+
